aefc5ffdcea1ccf775d0deaab15a52b69c809b90
pp86 <pietro.pinoli@gmail.com>
2020-05-29 17:56:39 +0200

    adds call to snpEFF

--- IlCodiCE.py
+++ IlCodiCE.py
@@ -1,208 +1,160 @@
 import numpy as np
+from Bio import pairwise2
+import os
 
-def print_matrix(m):
-    righe = m.shape[0]
-    colonne = m.shape[1]
-    for r in range(righe):
-        print("\t".join([str(m[r,c]) for c in range(colonne)]))
-
-def add_variant(sequence_id, pos, length, original,  mutated, variant_type):
-#     print(sequence_id, pos, length, original,  mutated, variant_type)
-    return [sequence_id, pos, length, original,  mutated, variant_type]
-
-def alignment(sequence_id = 1, seq1 = "attaaaggtttataccttcccaggtaaca", seq2 = "acccdccdcdccaacaca", add_variant=add_variant):
-    variants=[]
-    indel_score = -1
-    match_score = 2
-    mismatch_score = -1
-
-    limit = int(1.5*(abs(len(seq1) - len(seq2)) + 1))
-    limit = max(limit, 100)
-    limit = min(limit, len(seq1), len(seq2))
-
-    use_both = 0
-    use_seq1 = 1
-    use_seq2 = 2
-    empty = 3
-
-    seq1 = seq1
-    seq2 = seq2
-
-
-
-    scores = np.zeros((len(seq2)+1,len(seq1)+1))
-    scores.fill(-1000000)
-    traces=np.zeros((len(seq2)+1,len(seq1)+1),dtype=object)
-
-    #print("Matrices created")
-
-    #for i in range(len(seq2)+1):
-    #    for j in range(len(seq1)+1):
-    #        traces[i,j] = []
-    traces.fill(empty)
-
-    #print("Trace initialized")
-
-    for i in range(len(seq1)+1):
-        scores[0,i] = i*indel_score
-        if (i > 0):
-            traces[0,i] = use_seq1
-
-    for i in range(len(seq2)+1):
-        scores[i,0] = i*indel_score
-        if (i > 0):
-            traces[i,0] = use_seq2
-
-    #print("Scores initialized")
-
-    #print_matrix(scores)
-
-    for rs  in range(len(seq2)):
-    #    if rs % 100 == 0:
-    #        print(rs)
-        for cs in range(max(0, rs-limit), min(len(seq1), rs+limit)):
+def add_variant(sequence_id, pos_ref, pos_seq,  length, original,  mutated, variant_type, reference, sequence):
+    #return [sequence_id, pos_ref, pos_seq, length, original,  mutated, variant_type]
+    if variant_type == "INS":
+        return "\t".join(
+            map(str, ["NC_045512", 
+                      max(1,pos_ref), 
+                      ".", 
+                      reference[max(1,pos_ref)-1], 
+                      sequence[0:length+1] if (pos_ref == 0) else sequence[pos_seq-2:pos_seq-1+length], 
+                      ".", 
+                      ",".join(map(str, [variant_type,pos_seq,length, original, mutated])) ]))
+    elif variant_type == "DEL":
+        return "\t".join(
+            map(str, ["NC_045512", 
+                      pos_ref, 
+                      ".", 
+                      reference[0:length+1] if (pos_seq == 0) else reference[pos_ref-2:pos_ref-1+length], 
+                      sequence[max(1,pos_seq)-1], 
+                      ".", 
+                      ",".join(map(str, [variant_type,pos_seq,length, original, mutated])) ]))
+        
+    else:
+        return "\t".join(
+            map(str, ["NC_045512", 
+                      pos_ref, 
+                      ".", 
+                      original, 
+                      mutated, 
+                      ".", 
+                      ",".join(map(str, [variant_type,pos_seq,length, original, mutated])) ]))
 
 
-            rm = rs+1
-            cm = cs+1
-
-            score_use_seq1 = scores[rm, cm - 1] + indel_score
-            score_use_seq2 = scores[rm - 1, cm] + indel_score
-            score_use_both = scores[rm - 1, cm - 1] + (match_score if seq1[cs]==seq2[rs] else mismatch_score)
-
-            if score_use_both >= max(score_use_seq1, score_use_seq2):
-                scores[rm, cm] = score_use_both
-                traces[rm,cm] = use_both
-            if score_use_seq1 >= max(score_use_seq2, score_use_both):
-                scores[rm, cm] = score_use_seq1
-                traces[rm,cm] = use_seq1
-            if score_use_seq2 >= max(score_use_seq1, score_use_both):
-                scores[rm, cm] = score_use_seq2
-                traces[rm,cm] = use_seq2
-
-
-    #print_matrix(scores)
-
-    seq1_aligned = ""
-    seq2_aligned = ""
-
-    cm = len(seq1)
-    rm = len(seq2)
-
-    while not(cm == 0 and rm == 0):
-        action = traces[rm, cm]
-        if action == use_both:
-            if seq1[cm-1] == seq2[rm-1]:
-                seq1_aligned += seq1[cm-1]
-                seq2_aligned += seq2[rm-1]
+def create_aligner_to_reference(reference):
+    
+    def inner_fun(sequence, sequence_id = 1):
+        variants = []
+        
+        alignments = pairwise2.align.globalms(reference, sequence, 2, -1, -1, -.5)
+        ref_aligned = alignments[0][0]
+        seq_aligned = alignments[0][1]
+        
+        ref_positions = np.zeros(len(seq_aligned), dtype=int)
+        pos = 0
+        for i in range(len(ref_aligned)):
+            if ref_aligned[i] != '-':
+                pos +=1
+            ref_positions[i] = pos
+            
+        seq_positions = np.zeros(len(seq_aligned), dtype=int)
+        pos = 0
+        for i in range(len(seq_aligned)):
+            if seq_aligned[i] != '-':
+                pos += 1
+            seq_positions[i] = pos
+           
+        ins_open = False
+        ins_len = 0
+        ins_pos = None
+        ins_pos_seq = None
+        ins_seq = ""
+        for i in range(len(ref_aligned)):
+            if ref_aligned[i] == '-':
+                if not ins_open:
+                    ins_pos_seq = seq_positions[i]
+                ins_open = True
+                ins_len += 1
+                ins_pos = ref_positions[i]
+                ins_seq += seq_aligned[i]
             else:
-                seq1_aligned += seq1[cm - 1].upper()
-                seq2_aligned += seq2[rm - 1].upper()
-            cm -= 1
-            rm -= 1
-        elif action == use_seq1:
-            seq1_aligned += seq1[cm - 1]
-            seq2_aligned += "-"
-            cm -= 1
-        else:
-            seq1_aligned += "-"
-            seq2_aligned += seq2[rm - 1]
-            rm -=1
+                if ins_open:
+                    v = add_variant(sequence_id, ins_pos, ins_pos_seq, ins_len, "-" * ins_len, ins_seq, "INS", reference, sequence)
+                    variants.append(v)
 
-    seq1_aligned = seq1_aligned[::-1]
-    seq2_aligned = seq2_aligned[::-1]
+                    ins_open = False
+                    ins_len = 0
+                    ins_pos = None
+                    ins_pos_seq = None
+                    ins_seq = ""
+        if ins_open:
+            v = add_variant(sequence_id, ins_pos, ins_pos_seq, ins_len, "-" * ins_len, ins_seq, "INS", reference, sequence)
+            variants.append(v)
+            
+        del_open = False
+        del_len = 0
+        del_pos = None
+        del_pos_seq = None
+        del_seq = ""
+        for i in range(len(ref_aligned)):
+            if seq_aligned[i] == '-':
+                if not del_open:
+                    del_pos = ref_positions[i]
+                del_pos_seq = seq_positions[i]
+                del_open = True
+                del_len += 1
+                del_seq += ref_aligned[i]
+            else:
+                if del_open:
+                    v = add_variant(sequence_id, del_pos, del_pos_seq, del_len, del_seq, "-" * del_len, "DEL", reference, sequence)
+                    variants.append(v)
 
-    #print(len(seq1_aligned))
-    #print(len(seq2_aligned))
+                    del_open = False
+                    del_len = 0
+                    del_pos = None
+                    del_pos_seq = None
+                    del_seq = ""
 
-    #for i in range(0, (len(seq1_aligned)//50)+1):
-    #    print(seq1_aligned[i*50:(i+1)*50])
-    #    print(seq2_aligned[i * 50:(i + 1) * 50])
-    #    print()
+        if del_open:
+            v = add_variant(sequence_id, del_pos, del_pos_seq, del_len, del_seq, "-" * del_len, "DEL", reference, sequence)
+            variants.append(v)
+            
+        mut_open = False
+        mut_len = 0
+        mut_pos = None
+        mut_pos_seq = None
+        mut_seq_original = ""
+        mut_seq_mutated = ""
+        for i in range(len(ref_aligned)):
+            if ref_aligned[i] != '-' and seq_aligned[i] != '-' and ref_aligned[i] != seq_aligned[i]:
+                if not mut_open:
+                    mut_pos = ref_positions[i]
+                    mut_pos_seq = seq_positions[i]
+                mut_open = True
+                mut_len += 1
+                mut_seq_original += ref_aligned[i]
+                mut_seq_mutated += seq_aligned[i]
+            else:
+                if mut_open:
+                    v = add_variant(sequence_id, mut_pos, mut_pos_seq, mut_len, mut_seq_original, mut_seq_mutated, "SUB", reference, sequence)
+                    variants.append(v)
 
-    ref_positions = np.zeros(len(seq1_aligned), dtype=int)
-    pos = 0
-    for i in range(len(seq1_aligned)):
-        if seq1_aligned[i] != '-':
-            pos +=1
-        ref_positions[i] = pos
+                    mut_open = False
+                    mut_len = 0
+                    mut_pos = None
+                    mut_pos_seq = None
+                    mut_seq_original = ""
+                    mut_seq_mutated = ""
 
-    #print(ref_positions)
-    ins_open = False
-    ins_len = 0
-    ins_pos = None
-    ins_seq = ""
-    for i in range(len(seq1_aligned)):
-        if seq1_aligned[i] == '-':
-            ins_open = True
-            ins_len += 1
-            ins_pos = ref_positions[i]
-            ins_seq += seq2_aligned[i]
-        else:
-            if ins_open:
-                v = add_variant(sequence_id, ins_pos, ins_len, "-"*ins_len, ins_seq, "INS")
-                variants.append(v)
-                
-                ins_open = False
-                ins_len = 0
-                ins_pos = None
-                ins_seq = ""
-    if ins_open:
-        v = add_variant(sequence_id, ins_pos, ins_len, "-"*ins_len, ins_seq, "INS")
-        variants.append(v)
-
-    del_open = False
-    del_len = 0
-    del_pos = None
-    del_seq = ""
-    for i in range(len(seq1_aligned)):
-        if seq2_aligned[i] == '-':
-            if not del_open:
-                del_pos = ref_positions[i]
-            del_open = True
-            del_len += 1
-            del_seq += seq1_aligned[i]
-        else:
-            if del_open:
-                v = add_variant(sequence_id, del_pos, del_len, del_seq, "-"*del_len, "DEL")
-                variants.append(v)
-                
-                del_open = False
-                del_len = 0
-                del_pos = None
-                del_seq = ""
-
-    if del_open:
-        v = add_variant(sequence_id, del_pos, del_len, del_seq, "-"*del_len, "DEL")
-        variants.append(v)
-
-
-    mut_open = False
-    mut_len = 0
-    mut_pos = None
-    mut_seq_original = ""
-    mut_seq_mutated = ""
-    for i in range(len(seq1_aligned)):
-        if seq1_aligned[i] != '-' and seq2_aligned[i] != '-' and seq1_aligned[i] != seq2_aligned[i]:
-            if not mut_open:
-                mut_pos = ref_positions[i]
-            mut_open = True
-            mut_len += 1
-            mut_seq_original += seq1_aligned[i]
-            mut_seq_mutated += seq2_aligned[i]
-        else:
-            if mut_open:
-                v = add_variant(sequence_id, mut_pos, mut_len, mut_seq_original.lower(), mut_seq_mutated.lower(), "SUB")
-                variants.append(v)
-                
-                mut_open = False
-                mut_len = 0
-                mut_pos = None
-                mut_seq_original = ""
-                mut_seq_mutated = ""
-
-    if mut_open:
-        v = add_variant(sequence_id, mut_pos, mut_len, mut_seq_original.lower(), mut_seq_mutated.lower(), "SUB")
-        variants.append(v)
-    return variants
-alignment()
+        if mut_open:
+            v = add_variant(sequence_id, mut_pos, mut_pos_seq, mut_len, mut_seq_original, mut_seq_mutated, "SUB", reference, sequence)
+            variants.append(v)
+            
+        
+        with open("./tmp_snpeff/{}.vcf".format(sequence_id), "w") as f:
+            for m in variants:
+                f.write(m+'\n')
+            
+        os.system("java -jar ./tmp_snpeff/snpEff/snpEff.jar  covid  ./tmp_snpeff/MN000998.vcf > ./tmp_snpeff/output_{}.vcf".format(sequence_id, sequence_id))
+        
+        
+        with open("./tmp_snpeff/output_{}.vcf".format(sequence_id)) as f:
+            annotated_variants = [line for line in f if not line.startswith("#")]
+        
+        return annotated_variants
+        
+    
+    return inner_fun
