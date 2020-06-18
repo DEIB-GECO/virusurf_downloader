4300f6a12b8a99422f15a5f9650ac5f3e1dd1856
pp86 <pietro.pinoli@gmail.com>
2020-06-18 14:37:25 +0200

    Adds the "call_aa_variants" method to call amminoacid variants. \nDisables sequence annotations and aminoacid sequence generation for GenBank sequences

--- IlCodiCE.py
+++ IlCodiCE.py
@@ -37,7 +37,7 @@ def add_variant(sequence_id, pos_ref, pos_seq,  length, original,  mutated, vari
                       ",".join(map(str, [variant_type,pos_seq,length, original, mutated])) ]))
 
 
-def create_aligner_to_reference(reference, annotation_file):
+def create_aligner_to_reference(reference, annotation_file, is_gisaid = False):
     
     table = CodonTable.ambiguous_dna_by_id[1]
     reference_annotations = []
@@ -86,68 +86,69 @@ def create_aligner_to_reference(reference, annotation_file):
             seq_positions[i] = pos
         
         paired_positions = list(zip(ref_positions, seq_positions))
-        for a in reference_annotations:
-            name = a[0]
-            ann_type = "CDS"
-            start_ref = a[1]
-            stop_ref = a[2]
-            ann_pos = [s for (r,s) in paired_positions if r >= start_ref and r <= stop_ref]
-            seq_start = ann_pos[0]
-            seq_stop = ann_pos[-1]
-            dna_seq = sequence[seq_start-1:seq_stop].replace("-", "")
-            dna_ref = reference[start_ref-1:stop_ref]
-            aa_variants_gene = []
-            
-            try:
-                aa_ref = Seq._translate_str(dna_ref, table, cds=True)
-                aa_seq = Seq._translate_str(dna_seq, table, cds=True)
-                alignment_aa = pairwise2.align.globalms(aa_ref, aa_seq, 2, -1, -1, -.5)
-                
-                ref_aligned_aa = alignment_aa[0][0]
-                seq_aligned_aa = alignment_aa[0][1]
-                                
-                ref_positions_aa = np.zeros(len(seq_aligned_aa), dtype=int)
-                pos = 0
-                for i in range(len(ref_aligned_aa)):
-                    if ref_aligned_aa[i] != '-':
-                        pos +=1
-                    ref_positions_aa[i] = pos
-                    
-                aligned_aa = list(zip(ref_positions_aa, ref_aligned_aa, seq_aligned_aa))
-                mut_set = set()
-                for t in aligned_aa:
-                    if t[2] == "-":
-                        mutpos = t[0]
-                        original = t[1]
-                        alternative = t[2]
-                        mut_type = "DEL"
-                        mut_set.add((name, mutpos, original, alternative, mut_type))
-                    elif t[1] != t[2] :
-                        mutpos = t[0]
-                        original = [r for (p,r,s) in aligned_aa if p == mutpos if r!="-"][0]
-                        alternative = "".join([s for (p,r,s) in aligned_aa if p == mutpos])
-                        mut_type = "SUB"
-                        mut_set.add((name, mutpos, original, alternative, mut_type))
-                    elif t[1] == "-":
-                        mutpos = t[0]
-                        original = t[1]
-                        alternative = t[2]
-                        mut_type = "SUB"
-                        mut_set.add((name, mutpos, original, alternative, mut_type))
-                for mut in mut_set:
-                    aa_variants.append(mut)
-                    aa_variants_gene.append(mut)
-                        
-                    
-                
-                
-            except:
-                seq_start = None
-                seq_stop = None
-                aa_seq = None
-                
-            annotations.append((name, ann_type, seq_start, seq_stop, aa_seq, aa_variants_gene))
-           
+        if is_gisaid:
+            for a in reference_annotations:
+                name = a[0]
+                ann_type = "CDS"
+                start_ref = a[1]
+                stop_ref = a[2]
+                ann_pos = [s for (r,s) in paired_positions if r >= start_ref and r <= stop_ref]
+                seq_start = ann_pos[0]
+                seq_stop = ann_pos[-1]
+                dna_seq = sequence[seq_start-1:seq_stop].replace("-", "")
+                dna_ref = reference[start_ref-1:stop_ref]
+                aa_variants_gene = []
+
+                try:
+                    aa_ref = Seq._translate_str(dna_ref, table, cds=True)
+                    aa_seq = Seq._translate_str(dna_seq, table, cds=True)
+                    alignment_aa = pairwise2.align.globalms(aa_ref, aa_seq, 2, -1, -1, -.5)
+
+                    ref_aligned_aa = alignment_aa[0][0]
+                    seq_aligned_aa = alignment_aa[0][1]
+
+                    ref_positions_aa = np.zeros(len(seq_aligned_aa), dtype=int)
+                    pos = 0
+                    for i in range(len(ref_aligned_aa)):
+                        if ref_aligned_aa[i] != '-':
+                            pos +=1
+                        ref_positions_aa[i] = pos
+
+                    aligned_aa = list(zip(ref_positions_aa, ref_aligned_aa, seq_aligned_aa))
+                    mut_set = set()
+                    for t in aligned_aa:
+                        if t[2] == "-":
+                            mutpos = t[0]
+                            original = t[1]
+                            alternative = t[2]
+                            mut_type = "DEL"
+                            mut_set.add((name, mutpos, original, alternative, mut_type))
+                        elif t[1] != t[2] :
+                            mutpos = t[0]
+                            original = [r for (p,r,s) in aligned_aa if p == mutpos if r!="-"][0]
+                            alternative = "".join([s for (p,r,s) in aligned_aa if p == mutpos])
+                            mut_type = "SUB"
+                            mut_set.add((name, mutpos, original, alternative, mut_type))
+                        elif t[1] == "-":
+                            mutpos = t[0]
+                            original = t[1]
+                            alternative = t[2]
+                            mut_type = "SUB"
+                            mut_set.add((name, mutpos, original, alternative, mut_type))
+                    for mut in mut_set:
+                        aa_variants.append(mut)
+                        aa_variants_gene.append(mut)
+
+
+
+
+                except:
+                    seq_start = None
+                    seq_stop = None
+                    aa_seq = None
+
+                annotations.append((name, ann_type, seq_start, seq_stop, aa_seq, aa_variants_gene))
+
         ins_open = False
         ins_len = 0
         ins_pos = None
@@ -252,6 +253,51 @@ def create_aligner_to_reference(reference, annotation_file):
         
     
     return inner_fun
+
+
+def call_aa_variants(aa_ref, aa_seq, name):
+    
+    aa_variants = []
+    alignment_aa = pairwise2.align.globalms(aa_ref, aa_seq, 2, -1, -1, -.5)
+    ref_aligned_aa = alignment_aa[0][0]
+    seq_aligned_aa = alignment_aa[0][1]
+
+    ref_positions_aa = np.zeros(len(seq_aligned_aa), dtype=int)
+    pos = 0
+    for i in range(len(ref_aligned_aa)):
+        if ref_aligned_aa[i] != '-':
+            pos +=1
+        ref_positions_aa[i] = pos
+
+    aligned_aa = list(zip(ref_positions_aa, ref_aligned_aa, seq_aligned_aa))
+    mut_set = set()
+    for t in aligned_aa:
+        if t[2] == "-":
+            mutpos = t[0]
+            original = t[1]
+            alternative = t[2]
+            mut_type = "DEL"
+            mut_set.add((name, mutpos, original, alternative, mut_type))
+        elif t[1] != t[2] :
+            mutpos = t[0]
+            original = [r for (p,r,s) in aligned_aa if p == mutpos if r!="-"][0]
+            alternative = "".join([s for (p,r,s) in aligned_aa if p == mutpos])
+            mut_type = "SUB"
+            mut_set.add((name, mutpos, original, alternative, mut_type))
+        elif t[1] == "-":
+            mutpos = t[0]
+            original = t[1]
+            alternative = t[2]
+            mut_type = "SUB"
+            mut_set.add((name, mutpos, original, alternative, mut_type))
+    for mut in mut_set:
+        aa_variants.append(mut)
+        
+    
+    return aa_variants
+    
+    
+    
 
 
 def parse_annotated_variants(annotated_variants):
