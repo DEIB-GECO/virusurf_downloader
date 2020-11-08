076b4c0e764623fe5dbe50ecce36ef352e92c3c7
pp86 <pietro.pinoli@gmail.com>
2020-11-08 18:49:37 +0100

    minor changes

--- pangolin_script.sh
+++ pangolin_script.sh
@@ -1,7 +1,4 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pangolin
 echo "USAGE: bash pangoline_script.sh input.fasta outputDir outputFile"
-echo $1
-echo $2
-echo $3 
 pangolin $1 -o $2 --outfile $3 
