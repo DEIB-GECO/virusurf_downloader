97bbb8de0ba76457426dc51f1a39080d3c8809f6
Tommaso Alfonsi <tommaso.alfonsi@mail.polimi.it>
2021-01-25 17:42:25 +0100

    fix typos in sql queries on constraints and epitopes

--- sql_scripts/insert_linear_epitopes_into_epi_fragments.sql
+++ sql_scripts/insert_linear_epitopes_into_epi_fragments.sql
@@ -1,5 +1,5 @@
 -- This script is called automatically after each epitope import process.
-insert into epitope_fragment(epitope_id, epi_fragment_sequence, epi_frag_annotation_start, epi_frag_annotation_stop)
-select e.epitope_id, e.epitope_sequence, e.epi_annotation_start, e.epi_annotation_stop
-from epitope e
+insert into epitope_fragment(epitope_id, epi_fragment_sequence, epi_frag_annotation_start, epi_frag_annotation_stop) 
+select e.epitope_id, e.epitope_sequence, e.epi_annotation_start, e.epi_annotation_stop 
+from epitope e 
 where e.epitope_id not in (select distinct ef.epitope_id from epitope_fragment ef);
