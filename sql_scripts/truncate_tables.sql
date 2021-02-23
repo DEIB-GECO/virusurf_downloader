ea123e260f3ecc3710e3ef87e3bd27fc1ddb1153
Tommaso Alfonsi <tommaso.alfonsi@polimi.it>
2021-02-23 11:02:07 +0100

    prevents violation of foreign key when truncating tables

--- sql_scripts/truncate_tables.sql
+++ sql_scripts/truncate_tables.sql
@@ -1,6 +1,8 @@
-TRUNCATE db_meta, aminoacid_variant, annotation, variant_impact, nucleotide_variant, annotation_sequence,
-    nucleotide_sequence,
-    sequence,
+TRUNCATE db_meta,
+    aminoacid_variant, annotation_sequence, annotation,
+    variant_impact, nucleotide_variant,
+    overlap,
+    nucleotide_sequence, sequence,
     epitope_fragment, epitope,
     host_sample, experiment_type, sequencing_project,
-    host_specie, virus, overlap RESTART IDENTITY;
+    host_specie, virus RESTART IDENTITY;
